a35bb47a8b02accaccd37454fc627b51e1f3f0c7
gokulnath7820-jpg <gokulnath7820@gmail.com>
2026-01-12 16:39:48 +0530

    changes file

--- login.py
+++ login.py
@@ -1,35 +1,29 @@
 from tkinter import *
 from tkinter import messagebox
 
-
-window = Tk()
-window.title("Login form")
-window.geometry('340x440')
-window.configure(bg='#333333')
+root = Tk()
+root.title("Login form")
+root.geometry('350x370')
+root.configure(bg='grey')
 
 def login():
     username = "gokul"
     password = "12345"
     if username_entry.get()==username and password_entry.get()==password:
-        messagebox.showinfo(title="Login Success", message="You successfully logged in.")
+        messagebox.showinfo(title="Login Success", message="You successfully login.")
     else:
         messagebox.showerror(title="Error", message="Invalid login.")
 
 frame = Frame(bg='#333333')
 
-# Creating widgets
-login_label = Label(
-    frame, text="Login", bg='#333333', fg="#FF3399", font=("Arial", 30))
-username_label = Label(
-    frame, text="Username", bg='#333333', fg="#FFFFFF", font=("Arial", 16))
+login_label = Label(frame, text="Login", bg="#63B3EB", fg="white", font=("Arial", 30))
+username_label = Label(frame, text="Username", bg='#333333', fg="#FFFFFF", font=("Arial", 16))
 username_entry = Entry(frame, font=("Arial", 16))
 password_entry = Entry(frame, show="*", font=("Arial", 16))
-password_label = Label(
-    frame, text="Password", bg='#333333', fg="#FFFFFF", font=("Arial", 16))
-login_button = Button(
-    frame, text="Login", bg="#FF3399", fg="#FFFFFF", font=("Arial", 16), command=login)
+password_label = Label(frame, text="Password", bg='#333333', fg="#FFFFFF", font=("Arial", 16))
+login_button = Button(frame, text="Login", bg="skyblue", fg="#FFFFFF", font=("Arial", 16), command=login)
 
-# Placing widgets on the screen
+
 login_label.grid(row=0, column=0, columnspan=2, sticky="news", pady=40)
 username_label.grid(row=1, column=0)
 username_entry.grid(row=1, column=1, pady=20)
@@ -38,5 +32,4 @@ password_entry.grid(row=2, column=1, pady=20)
 login_button.grid(row=3, column=0, columnspan=2, pady=30)
 
 frame.pack()
-
-window.mainloop()
+root.mainloop()
